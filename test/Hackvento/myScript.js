ea509b8d35ac2a4122751e3df5e3ea5224ac0859
Kaushal <kaushallokhande12345@gamil.com>
2023-10-14 20:57:36 +0530

    Project Hosted on github

---
+++ test/Hackvento/myScript.js
@@ -0,0 +1,14 @@
+// Create a JavaScript file, e.g., myScript.js
+
+// Function to handle the button click event
+function goToMessagePage() {
+    // You can use window.location to navigate to the "message.html" page
+    window.location.href = "Admin.html";
+  }
+  
+  // // Attach the function to the button's click event
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   var button = document.querySelector(".btn");
+  //   button.addEventListener("click", goToMessagePage);
+  // });
+  
