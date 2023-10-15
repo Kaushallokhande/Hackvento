e0de90160b3a1f71c0986562874450a8c0e900fb
Kaushal Lokhande <118427691+Kaushallokhande@users.noreply.github.com>
2023-10-15 20:25:59 +0530

    Delete test/Hackvento directory
    ..

--- test/Hackvento/complainhome.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("complaint-form")
-        .addEventListener("submit", function (e) {
-          e.preventDefault();
-          alert("Complaint submitted successfully!");
-        });
